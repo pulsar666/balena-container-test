d377aea4fee8a7287d4ffbe603858a9c10843e83
pulsar666 <rahaanirban91@gmail.com>
2023-07-27 15:48:57 +0530

    change

--- zt-init.sh
+++ zt-init.sh
@@ -8,11 +8,14 @@ mkdir -p /var/lib/zerotier-one
 iptables -A INPUT -p udp -m udp --dport 9993 -j ACCEPT
 iptables -A INPUT -p tcp -m tcp --dport 9993 -j ACCEPT
 
-# Start the ZeroTier service
-zerotier-one
+# Start the ZeroTier service and redirect the output to a log file
+zerotier-one -D > /var/log/zt-join.log 2>&1 &
 
-# Join the ZeroTier network using the provided network ID
-zerotier-cli join $ZT_NETWORK_ID
+# Wait for the ZeroTier service to start
+sleep 5
+
+# Join the ZeroTier network using the provided network ID and redirect the output to the log file
+zerotier-cli join $ZT_NETWORK_ID >> /var/log/zt-init.log 2>&1
 
 # Keep the container running
 tail -f /dev/null
